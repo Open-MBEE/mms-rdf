1778e6970eb09ef3f22f211d3da8da0c3dff11b5
Blake Regalia <blake.regalia@gmail.com>
2019-08-07 16:25:15 -0700

    s3 upload proxy neptune agent

--- src/action/upload-s3.js
+++ src/action/upload-s3.js
@@ -7,18 +7,28 @@ const aws = require('aws-sdk');
 
 let a_files = process.argv.slice(2);
 
+let w_agent = {
+	maxSockets: 64,
+};
+
+if(process.env.NEPTUNE_PROXY) {
+	let du_proxy = new url.URL(process.env.NEPTUNE_PROXY);
+
+	w_agent = new proxy_agent({
+		protocol: du_proxy.protocol,
+		host: du_proxy.host,
+		port: du_proxy.port,
+		...w_agent,
+	});
+}
+
 let y_s3 = new aws.S3({
 	region: 'us-east-2',
 	apiVersion: '2006-03-01',
 	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	httpOptions: {
-		agent: new proxy_agent({
-			protocol: 'socks:',
-			host: '127.0.0.1',
-			port: 3031,
-			maxSockets: 64,
-		}),
+		agent: w_agent,
 	},
 });
 
