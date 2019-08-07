1d63dd7dc9f4ed058317ad0bd8fd22d180ce66f0
Blake Regalia <blake.regalia@gmail.com>
2019-08-07 16:28:40 -0700

    http agent fix

--- src/action/upload-s3.js
+++ src/action/upload-s3.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const url = require('url');
+const http = require('http');
 
 const proxy_agent = require('proxy-agent');
 
@@ -7,19 +8,29 @@ const aws = require('aws-sdk');
 
 let a_files = process.argv.slice(2);
 
-let w_agent = {
+let d_agent = null;
+
+// default agent properties
+let g_agent = {
 	maxSockets: 64,
 };
 
+// neptune proxy env var set
 if(process.env.NEPTUNE_PROXY) {
+	// parse proxy url
 	let du_proxy = new url.URL(process.env.NEPTUNE_PROXY);
 
-	w_agent = new proxy_agent({
+	// create proxy agent
+	d_agent = new proxy_agent({
 		protocol: du_proxy.protocol,
 		host: du_proxy.host,
 		port: du_proxy.port,
-		...w_agent,
+		...g_agent,
 	});
+}
+// proxy not set
+else {
+	d_agent = new http.Agent(g_agent);
 }
 
 let y_s3 = new aws.S3({
@@ -28,7 +39,7 @@ let y_s3 = new aws.S3({
 	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	httpOptions: {
-		agent: w_agent,
+		agent: d_agent,
 	},
 });
 
